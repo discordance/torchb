c440749a0592673b6dde55d1160ce2001ef84428
Aymeric Nunge <nunja@free.fr>
2017-09-15 00:05:22 +0200

    tf is life

--- encode_beats_TF.py
+++ encode_beats_TF.py
@@ -12,9 +12,9 @@ collection = db.origset
 nm = 7
 beats = list(collection.find({'class':6,
                               'bar': 128,
-                              'gridicity': {'$lt': 0.4},
+                              'gridicity': {'$lt': 0.5},
                               'diversity': {'$gt': 0.07}
-                              }).limit(1002))
+                              }).limit(104))
 
 # decompress in numpy
 alll = []
@@ -37,9 +37,6 @@ alll_f = alll_f.reshape((alll_f.shape[0],alll_f.shape[1]*20,))
 # okay
 print "dataset: ", alll_f.shape
 
-train_batch_size = 32
-valid_batch_size = 32
-
 # manage data loading
 train_size = (alll_f.shape[0]/4)*3
 val_size = (alll_f.shape[0]/4)
@@ -47,16 +44,15 @@ val_size = (alll_f.shape[0]/4)
 print "train/valid sets size: ", train_size, val_size, "\n"
 
 # Training Parameters
-learning_rate = 0.01
-num_steps = 30000
-batch_size = 256
+learning_rate = 0.001
+num_steps = 60000
+batch_size = 128
 
-display_step = 100
-examples_to_show = 10
+display_step = 1000
 
 # Network Parameters
-num_hidden_1 = 256 # 1st layer num features
-num_hidden_2 = 128 # 2nd layer num features (the latent dim)
+num_hidden_1 = 768 # 1st layer num features
+num_hidden_2 = 128 # 2nd layer num features
 num_input = alll_f.shape[1]
 
 # tf Graph input (only pictures)
@@ -77,23 +73,26 @@ biases = {
 
 # Building the encoder
 def encoder(x):
-    # Encoder Hidden layer with relu activation #1
-    layer_1 = tf.nn.relu(tf.add(tf.matmul(x, weights['encoder_h1']),
-                                   biases['encoder_b1']))
-    # Encoder Hidden layer with relu activation #2
-    layer_2 = tf.nn.relu(tf.add(tf.matmul(layer_1, weights['encoder_h2']),
-                                   biases['encoder_b2']))
+    # Encoder Hidden layer
+    l1_op = tf.add(tf.matmul(x, weights['encoder_h1']), biases['encoder_b1'])
+    # @FIXEME have to manage the training param
+    l1_op = tf.layers.batch_normalization(l1_op, training=True)
+    layer_1 = tf.nn.relu(l1_op)
+    # Encoder Hidden layer
+    l2_op = tf.add(tf.matmul(layer_1, weights['encoder_h2']), biases['encoder_b2'])
+    # @FIXEME have to manage the training param
+    l2_op = tf.layers.batch_normalization(l2_op, training=True)
+    layer_2 = tf.nn.relu(l2_op)
+
     return layer_2
 
 
 # Building the decoder
 def decoder(x):
-    # Decoder Hidden layer with sigmoid activation #1
-    layer_1 = tf.nn.relu(tf.add(tf.matmul(x, weights['decoder_h1']),
-                                   biases['decoder_b1']))
-    # Decoder Hidden layer with sigmoid activation #2
-    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']),
-                                   biases['decoder_b2']))
+    # Decoder Hidden layer
+    layer_1 = tf.nn.relu(tf.add(tf.matmul(x, weights['decoder_h1']),biases['decoder_b1']))
+    # Decoder Hidden layer
+    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']),biases['decoder_b2']))
     return layer_2
 
 
@@ -108,7 +107,7 @@ y_true = X
 
 
 # Define loss and optimizer, minimize the squared error
-loss = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
+loss = tf.reduce_mean(tf.pow(y_true - y_pred, 4))
 # loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=y_true, logits=y_pred))
 optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
 
@@ -127,6 +126,7 @@ def next_batch(num, data, labels):
 
     return np.asarray(data_shuffle), np.asarray(labels_shuffle)
 
+saver = tf.train.Saver()
 # Start Training
 # Start a new TF session
 with tf.Session() as sess:
@@ -143,9 +143,11 @@ with tf.Session() as sess:
         _, l = sess.run([optimizer, loss], feed_dict={X: batch_x})
         # Display logs per step
         if i % display_step == 0 or i == 1:
-            print('Step %i: Minibatch Loss: %f' % (i, l))
             valid, _ = next_batch(1, alll_f, np.array([-1] * alll_f.shape[0]))
             g = sess.run(decoder_op, feed_dict={X: valid})
-
-            utils.np_seq2mid(valid[0])
-            utils.np_seq2mid(g[0])
+            print utils.draw(valid[0].reshape((128,20))) + "\n"
+            print utils.draw(g[0].reshape((128,20))) + "\n"
+            saver.save(sess, 'encoded/jazz1/jazz1_%i'%i)
+            print('Step %i: Minibatch Loss: %f' % (i, l))
+        if i % (display_step/8) == 0:
+            print('Step %i: Minibatch Loss: %f' % (i, l))
